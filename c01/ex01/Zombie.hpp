79a4619fa02165637e9dde6ab109cc8acc45ff4d
Benjamin Lee <benjamin.lee.2021@gmail.com>
2022-06-23 12:31:38 +0930

    feat(c01/ex01): created Multi Zombies

---
+++ c01/ex01/Zombie.hpp
@@ -0,0 +1,27 @@
+#ifndef ZOMBIE_HPP
+
+#define ZOMBIE_HPP
+#include <iostream>
+#include <algorithm>
+
+/*
+    Create the Zombie class definition.
+*/
+class Zombie
+{
+private:
+    std::string _name;
+
+public:
+    Zombie(std::string name);
+    Zombie(void);
+    ~Zombie(void);
+    void announce(void);
+    void setName(std::string name);
+};
+
+Zombie *newZombie(std::string name);
+
+void randomChump(std::string name);
+
+#endif
